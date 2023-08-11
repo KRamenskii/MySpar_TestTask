746a48c7fc103e0224fdb3677831a32ff83d27c7
Ramenskii Kirill <kesha-1999-kirill@mail.ru>
2023-08-11 15:42:08 +0700

    Initial Commit

---
+++ MySpar_TestTask/MySpar_TestTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySpar_TestTaskApp.swift
+//  MySpar_TestTask
+//
+//  Created by Кирилл Раменский on 11.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct MySpar_TestTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
